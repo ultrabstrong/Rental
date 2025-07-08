af1a460e727c1ce745b6e89b8a0aaad93e299221
Brandon <ultrabstrong@gmail.com>
2025-07-08 07:36:16 -0600

    Moved maintenance request to separate controller

---
+++ ApartmentWeb/Controllers/ControllerWithPdfSubmissionBase.cs
@@ -0,0 +1,26 @@
+﻿using ApartmentWeb.Extensions;
+using System.IO;
+using System.Web.Mvc;
+
+namespace ApartmentWeb.Controllers
+{
+    public abstract class ControllerWithPdfSubmissionBase : Controller
+    {
+        protected string RenderRazorViewToString(string viewName, object model)
+        {
+            ViewData.Model = model;
+            using (var sw = new StringWriter())
+            {
+                if (viewName == nameof(HomeController.Apply))
+                {
+                    this.AddUSStatesToViewBag();
+                }
+                ViewEngineResult viewResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
+                ViewContext viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+                viewResult.ViewEngine.ReleaseView(ControllerContext, viewResult.View);
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
--- ApartmentWeb/Controllers/HomeController.cs
+++ ApartmentWeb/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using ApartmentWeb.Extensions;
 using ApartmentWeb.Models;
 using ApartmentWeb.Models.Application;
-using ApartmentWeb.Models.Maintenance;
 using Domain.Core;
 using Serilog;
 using System;
@@ -11,7 +10,7 @@ using System.Web.Mvc;
 
 namespace ApartmentWeb.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : ControllerWithPdfSubmissionBase
     {
         public static readonly string Name = nameof(HomeController).Replace(nameof(Controller), "");
 
@@ -45,9 +44,6 @@ namespace ApartmentWeb.Controllers
             return PartialView("Apply", new Application()); // Use PartialView for AJAX
 #endif
         }
-
-        [HttpGet, Route("MaintenanceRequest")]
-        public ActionResult MaintenanceRequest() => View(new MaintenanceRequest());
 
         [HttpGet, Route("Privacy")]
         public ActionResult Privacy() => View();
@@ -96,64 +92,6 @@ namespace ApartmentWeb.Controllers
             }
 
             return Json(new SubmitResponse { isSuccess = true, redirectUrl = Url.Action(nameof(Index), Name) });
-        }
-
-        [HttpPost, Route("SubmitMaintenanceRequest")]
-        [ValidateAntiForgeryToken]
-        public ActionResult SubmitMaintenanceRequest(MaintenanceRequest maintenanceRequest)
-        {
-            try
-            {
-                if (!ModelState.IsValid)
-                {
-                    var errors = ModelState
-                        .Where(m => m.Value.Errors.Any())
-                        .Select(m => $"{m.Key} {string.Join(",", m.Value.Errors.Select(e => e.ErrorMessage))}");
-                    Log.Logger.Information("Maintenance request validation errors: {@Errors}", errors);
-                    return Json(new SubmitResponse { isSuccess = false, hasValidationErrors = true });
-                }
-
-                Log.Logger.Debug("Creating view HTML");
-                var html = RenderRazorViewToString("~/Views/Home/MaintenanceRequestPdf.cshtml", maintenanceRequest);
-
-                Log.Logger.Debug("Converting HTML to PDF");
-                var pdf = HtmlConverter.ToPdf(html,
-                    Shared.Configuration.CompanyName,
-                    $"{maintenanceRequest.FirstName} {maintenanceRequest.LastName} Maintenance Request",
-                    $"Maintenance request for {maintenanceRequest.RentalAddress} from {maintenanceRequest.FirstName} {maintenanceRequest.LastName}");
-
-                Log.Logger.Debug("Sending maintenance email");
-                using (var pdfStream = new MemoryStream(pdf))
-                using (var emailService = new EmailService(Shared.Configuration.MailSettings))
-                {
-                    emailService.SendEmail(maintenanceRequest, pdfStream);
-                }
-                Log.Logger.Debug("Finished sending maintenance email");
-            }
-            catch (Exception ex)
-            {
-                Log.Logger.Error(ex, "Failed to submit maintenance request");
-                return Json(new SubmitResponse() { isSuccess = false });
-            }
-
-            return Json(new SubmitResponse { isSuccess = true, redirectUrl = Url.Action(nameof(Index), Name) });
-        }
-
-        private string RenderRazorViewToString(string viewName, object model)
-        {
-            ViewData.Model = model;
-            using (var sw = new StringWriter())
-            {
-                if (viewName == nameof(Apply))
-                {
-                    this.AddUSStatesToViewBag();
-                }
-                ViewEngineResult viewResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
-                ViewContext viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
-                viewResult.View.Render(viewContext, sw);
-                viewResult.ViewEngine.ReleaseView(ControllerContext, viewResult.View);
-                return sw.GetStringBuilder().ToString();
-            }
         }
     }
 }
