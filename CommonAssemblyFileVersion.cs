546de5410553295bf1ab8a83a20083c70c79640b
Brandon <ultrabstrong@gmail.com>
2025-08-27 22:54:09 -0600

    Use new versioning props file

--- CommonAssemblyFileVersion.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCopyright("Copyright © ApexProperties 2025")]
-[assembly: AssemblyTrademark("Rental Website")]
-[assembly: AssemblyCompany("ApexProperties")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
