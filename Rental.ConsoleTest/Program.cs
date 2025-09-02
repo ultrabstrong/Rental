fbd9de729ca514b1b28c9a553628c3fa4112ee0d
Brandon <ultrabstrong@gmail.com>
2025-09-01 22:36:45 -0600

    Removed unused console app tests

--- Rental.ConsoleTest/Program.cs
+++ Rental.ConsoleTest/Program.cs
@@ -1,10 +1,4 @@
-﻿using Rental.Domain.Email.Models;
-using Rental.WebApp.Converters;
-using System;
-using System.IO;
-using System.Net;
-using System.Net.Mail;
-using System.Text.RegularExpressions;
+﻿using System;
 
 namespace Rental.ConsoleTest;
 
@@ -19,7 +13,6 @@ partial class Program
     {
         try
         {
-            TestHTMLtoPDF();
         }
         catch (Exception ex)
         {
@@ -28,63 +21,4 @@ partial class Program
         Console.WriteLine("Program finished. Press any key to exit");
         Console.ReadKey();
     }
-
-    static void TestEmailRegex()
-    {
-        var reg = EmailTestRegex();
-        var email = "ultrabstrong@gmail.com";
-        Console.WriteLine(reg.IsMatch(email));
-    }
-
-    static void TestHTMLtoPDF()
-    {
-        var filepath = $"{DownloadsLoc}sample.html";
-        var outpath = $"{DownloadsLoc}sample.pdf";
-        var html = File.ReadAllText(filepath);
-        var pdfBytes = HtmlToPdfConverter.GetPdfBytes(html, "Rental.WebApp.ConsoleTest", "Sample PDF", "This is a sample PDF generated from HTML");
-
-        if (File.Exists(outpath))
-        { File.Delete(outpath); }
-        using (FileStream fs = new(outpath, FileMode.Create, FileAccess.ReadWrite))
-        {
-            fs.Write(pdfBytes, 0, pdfBytes.Length);
-            fs.Flush();
-        }
-    }
-
-
-    static void TestMail()
-    {
-        EmailOptions settings = new()
-        {
-            SmtpServer = "",
-            SmtpUsername = "",
-            SmtpPw = "",
-            SmtpPort = 587,
-            SmtpTo = ""
-        };
-
-        using (var smtpClient = new SmtpClient(settings.SmtpServer)
-        {
-            Port = settings.SmtpPort,
-            Credentials = new NetworkCredential(settings.SmtpUsername, settings.SmtpPw),
-            EnableSsl = true,
-            DeliveryMethod = SmtpDeliveryMethod.Network
-        })
-        {
-            MailMessage message = new()
-            {
-                Subject = $"test subject",
-                From = new MailAddress(settings.SmtpUsername),
-                Body = $"test body",
-                IsBodyHtml = false,
-                To = { settings.SmtpTo }
-            };
-
-            smtpClient.Send(message);
-        }
-    }
-
-    [GeneratedRegex(@"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$")]
-    private static partial Regex EmailTestRegex();
 }
